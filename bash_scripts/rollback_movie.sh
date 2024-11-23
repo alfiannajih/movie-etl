7f976f920e26cd3a8b2823969e539899c496f502
Alfian <alfiannajih@gmail.com>
2024-11-23 18:11:43 +0700

    fix sql statements

--- bash_scripts/rollback_movie.sh
+++ bash_scripts/rollback_movie.sh
@@ -1,3 +1,6 @@
 source .env
 
-docker exec movie-db  psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB" -q -f /db_scripts/rollback_scripts/rollback_movie.sql -v movie_id="$1"
+for movie_id in "$@"
+do
+    psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST/$POSTGRES_DB" -q -f ./db_scripts/rollback_scripts/rollback_movie.sql -v movie_id="$movie_id"
+done
